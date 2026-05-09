305f46a5ea4643bdf675e622100d3bb695d30af0
RavinduUni <fc115680@foc.sjp.ac.lk>
2026-05-09 10:55:41 +0530

    implement-testing-n8n-webhook

--- server/controllers/n8nController.js
+++ server/controllers/n8nController.js
@@ -1,7 +1,8 @@
 export const getWebhook = async (req, res) => {
     try {
         const webhookData = req.body;
-        console.log(webhookData);
+        
+        return res.json({ success: true, message: "Webhook received successfully", data: webhookData });
     } catch (error) {
         res.status(500).json({ success: false, error: error.message });
     }
